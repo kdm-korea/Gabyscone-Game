5710f775080a39c8fe22ca3373a09546c55d3c7f
KDM_korea <kdm_korea@naver.com>
2019-08-30 20:07:07 +0900

    [#2] Feat: Create Unity project

---
+++ Gabyscone-Game/Assets/Script/CharacterMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    public float gravity = 0.0f;
+    public float speed = 0.0f;
+
+
+    private CharacterController character;
+
+    void Start() {
+
+    }
+
+    void Update() {
+        
+    }
+}
