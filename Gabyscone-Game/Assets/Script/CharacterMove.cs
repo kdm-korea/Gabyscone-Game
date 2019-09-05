f847a5f3beaa93ebb4b886f421c03d721592d71b
KDM_korea <kdm_korea@naver.com>
2019-09-05 23:40:11 +0900

    [#2] Feat: Character move script

--- Gabyscone-Game/Assets/Script/CharacterMove.cs
+++ Gabyscone-Game/Assets/Script/CharacterMove.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
-    public float gravity = 0.0f;
-    public float speed = 0.0f;
-
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
 
     private CharacterController character;
+    private Vector3 moveDirection = Vector3.zero;
 
     void Start() {
-
+        character = gameObject.AddComponent<CharacterController>();
     }
 
     void Update() {
-        
+        if (character.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump")) {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        character.Move(moveDirection * Time.deltaTime); 
     }
 }
